0ec99ba8ca9ba4cfbc3971a74f57ef1cdcc7adf9
Carlos Balladares <carlos@balladares.co>
2018-08-03 11:40:36 -0700

    initial commit

---
+++ src/main/java/com/carlosbf/MySpringBootReactApp/MySpringBootReactAppApplication.java
@@ -0,0 +1,12 @@
+package com.carlosbf.MySpringBootReactApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootReactAppApplication.class, args);
+	}
+}
